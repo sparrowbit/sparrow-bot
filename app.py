c961e08cbce767a0f10a94fa2871343e699b1c5d
harisheoran <harisheoran01@gmail.com>
2023-08-14 18:07:25 +0530

    added interactive event - Button Click

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from slack_bolt import App
 from dotenv import load_dotenv
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+
 # Load the variables from the .env file
 load_dotenv()
 
@@ -18,62 +19,54 @@ app = App(
 # check out listeners argument - https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
 @app.message("hello")
 def message_hello(message, say):
-  # say() will send the message where event is triggered
-  say(f"Hey there <@{message['user']}>! , How are you doing today?")
+    # say() will send the message where event is triggered
+    say(f"Hey there <@{message['user']}>! , How are you doing today?")
 
 
-# Add functionality here
-# @app.event("app_home_opened") etc
-@app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-  try:
-    # views.publish is the method that your app uses to push a view to the Home tab
-    client.views_publish(
-      # the user that opened your app's app home
-      user_id=event["user"],
-      # the view object that appears in the app home
-      view={
-        "type": "home",
-        "callback_id": "home_view",
+# Interactivity events
+# 01. First, we’ll send a message that contains an interactive component (in this case a button)
+# 02. Next, we’ll listen for the action of a user clicking the button before responding
+@app.message("hey")
+def message_hey(message, say):
+    # say() will send the message to the channel where event was triggered
+    say(
 
-        # body of the view
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This is Sparrow Bot Developed by Team Sparrow Bit"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "Hello there, this is Sparrow Bot. "
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Say Hello"
+        # The value inside of say() is now an object that contains an array of blocks.
+        # Blocks are the building components of a Slack message and can range from text to images to
+        # datepickers. In this case, your app will respond with a section block that includes a button as
+        # an accessory. Since were using blocks, the text is a fallback for notifications and accessibility.
+
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click"
                 }
-              }
-            ]
-          }
-        ]
-      }
+
+            }
+        ],
+        text=f"Hey there, <@{message['user']}>!"
     )
 
-  except Exception as e:
-    logger.error(f"Error publishing home tab: {e}")
 
+# Let’s add a handler to send a followup message when someone clicks the button, it need the "action_id" from accessories above
+@app.action("button_click")
+def button_click_action(body, ack, say):
+    # Acknowledge the action
+    ack()
+    say(f"<@{body['user']['id']}>clicked this button.")
+
+# LISTENING TO MESSAGES
+# listen to messages that arent type of messages like an emoji
+@app.message(":wave")
+def react_to_wave_emoji(message, say):
+  user = message['user']
+  say(f"hello there 👋 <@{user}>")
+  
+# LISTENING TO EVENTS
 
 # Start your app
 # this http server is using a built in development adapter which is reponsible for handling
