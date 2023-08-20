c7fa03cd61f4370ac732ff7e8d58f854e29236b3
harisheoran <harisheoran01@gmail.com>
2023-08-20 17:37:37 +0530

    added Dockerfile

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from slack_bolt import App
 # import env keys
 from dotenv import load_dotenv
-from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 
 # Load the variables from the .env file
